232e7363e9b532ddcf87d4ce475ea4b0bd07fce8
Madalin Duca <ducanul@gmail.com>
2014-04-30 12:38:06 +0200

    commited

---
+++ startpager/js/settings.js
@@ -0,0 +1,30 @@
+/* Settings */
+
+//Default layout for the grid - users who didn't save one will get this
+var default_grid_layout = "x-2/y1/facebook x-1/y1/reddit x0/y1/youtube x1/y1/bloglovin x2/y1/twitter x-2/y2/aliexpress x-1/y2/amazon x0/y2/booking x1/y2/ebay x2/y2/mrkate";
+
+// Grid box's vars - mostly used by renderGrid and some jQuery
+var box_width = 80;
+var box_height = 90;
+var box_margin = 10;  // The space between boxes
+var box_background_color = 'rgba(229, 229, 229, 0.5)';
+var indicator_box_background_color = 'rgba(0, 0, 0, 0.9)';
+
+var box_image_width = 70;
+var box_image_height = 70;
+
+//Number of max characters a string must be smaller or equal to in order to be in each category
+//Because anything longer than small size goes undisplayed, char_size_small is also max_char_size
+var box_char_size_big = '9';
+var box_char_size_medium = '10';
+var box_char_size_small = '15';
+//Font size for each string class
+var box_font_size_big = '14px';
+var box_font_size_medium = '12px';
+var box_font_size_small = '10px';
+
+//Settings for the icon menu scrollbar
+var menu_scrollbar_color = '#2b2b2b';
+var menu_scrollbar_rail_color = '#7c7c7c';
+
+/* End of settings */
